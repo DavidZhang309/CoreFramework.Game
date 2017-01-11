7619168023aa30c25493cce1287c1a6377412cfa
David Zhang <david_zhang_309@hotmail.com>
2017-01-10 21:45:08 -0500

    Added Event based mouse and keyboard support (taken from old code and should be reimplemented by taking input from windows events)

---
+++ Controls/Keyboard.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+
+namespace CoreFramework.Game.Controls
+{
+    public class KeyEventArgs : EventArgs
+    {
+        public KeyEventArgs(KeyboardState state, Keys[] events)
+        {
+            State = state;
+            ChangedKeys = events;
+        }
+
+        public KeyboardState State { get; private set; }
+        public Keys[] ChangedKeys { get; private set; }
+        public int Count
+        {
+            get { return ChangedKeys.Length; }
+        }
+    }
+    public class KeyBoardComponent
+    {
+        KeyboardState lastState;
+        Keys[] lastPressed;
+
+        public KeyboardState RawState { get; private set; }
+        public event EventHandler<KeyEventArgs> OnKeyDown;
+        public event EventHandler<KeyEventArgs> OnKeyUp;
+
+        public void Initialize()
+        {
+            this.lastState = Keyboard.GetState();
+            lastPressed = lastState.GetPressedKeys();
+        }
+
+        private bool Contains(Keys[] keys, Keys key)
+        {
+            for (int i = 0; i < keys.Length; i++)
+                if (keys[i] == key)
+                    return true;
+            return false;
+        }
+        public void Update(GameTime time)
+        {
+            Queue<Keys> changed = new Queue<Keys>();
+
+            KeyboardState current = Keyboard.GetState();
+            RawState = current;
+
+            Keys[] pressed = current.GetPressedKeys();
+
+            //tmp: fix missing change
+            if (lastPressed == null)
+            {
+                lastPressed = pressed;
+                return;
+            }
+
+            foreach (Keys key in pressed)
+                if (!Contains(lastPressed, key))
+                    changed.Enqueue(key);
+
+            if (changed.Count != 0 && OnKeyDown != null)
+            {
+                Keys[] events = new Keys[changed.Count];
+                for (int i = 0; i < events.Length; i++)
+                {
+                    events[i] = changed.Dequeue();
+                }
+                OnKeyDown(this, new KeyEventArgs(current, events));
+            }
+
+            foreach (Keys key in lastPressed)
+                if (!Contains(pressed, key))
+                    changed.Enqueue(key);
+
+            if (changed.Count != 0 && OnKeyUp != null)
+            {
+                Keys[] events = new Keys[changed.Count];
+                for (int i = 0; i < events.Length; i++)
+                {
+                    events[i] = changed.Dequeue();
+                }
+                OnKeyUp(this, new KeyEventArgs(current, events));
+            }
+
+            lastState = current;
+            lastPressed = pressed;
+        }
+    }
+}
---
+++ Controls/Mouse.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+
+namespace CoreFramework.Game.Controls
+{
+    public enum MInputType { LB, MB, RB, SCROLL, XB1, XB2, Position };
+    public class MouseEventArgs : EventArgs
+    {
+        private EventMouse mouse;
+
+        public MouseEventArgs(EventMouse Mouse)
+        {
+            mouse = Mouse;
+        }
+
+        public EventMouse Event
+        {
+            get { return mouse; }
+        }
+    }
+    /// <summary>
+    /// Provides a event based mouse input using MouseState 
+    /// </summary>
+    public class EventMouse
+    {
+        private MouseState state;
+        private MInputType inputType;
+        private object delta;
+
+        public EventMouse(MouseState State, MInputType InputType, object Delta)
+        {
+            state = State;
+            inputType = InputType;
+            delta = Delta;
+        }
+
+        public MouseState State
+        {
+            get { return state; }
+        }
+        /// <summary>
+        /// The type of input of the event
+        /// </summary>
+        public MInputType Input
+        {
+            get { return inputType; }
+        }
+        /// <summary>
+        /// Point if a change in X and/or Y, as (dx, dy)
+        /// int if scrolling, as (dx)
+        /// bool for button
+        /// </summary>
+        public object Delta
+        {
+            get { return delta; }
+        }
+    }
+    public class MouseComponent
+    {
+        MouseState lastState;
+
+        public MouseState RawState { get; private set; }
+
+        public event EventHandler<MouseEventArgs> MouseEvent;
+
+        public void Initialize()
+        {
+            lastState = Mouse.GetState();
+        }
+
+        public void Update(GameTime time)
+        {
+            MouseState current = Mouse.GetState();
+            RawState = current;
+            EventMouse currentEvent = null;
+
+            if (lastState.LeftButton != current.LeftButton)
+                currentEvent = new EventMouse(current, MInputType.LB, current.LeftButton == ButtonState.Pressed);
+            if (lastState.MiddleButton != current.MiddleButton)
+                currentEvent = new EventMouse(current, MInputType.MB, current.MiddleButton == ButtonState.Pressed);
+            if (lastState.RightButton != current.RightButton)
+                currentEvent = new EventMouse(current, MInputType.RB, current.RightButton == ButtonState.Pressed);
+            if (lastState.XButton1 != current.XButton1)
+                currentEvent = new EventMouse(current, MInputType.XB1, current.XButton1 == ButtonState.Pressed);
+            if (lastState.XButton2 != current.XButton2)
+                currentEvent = new EventMouse(current, MInputType.XB2, current.XButton2 == ButtonState.Pressed);
+            if (lastState.ScrollWheelValue != current.ScrollWheelValue)
+                currentEvent = new EventMouse(current, MInputType.SCROLL, current.ScrollWheelValue - lastState.ScrollWheelValue);
+            if (lastState.X != current.X || lastState.Y != current.Y)
+                currentEvent = new EventMouse(current, MInputType.Position, new Point(current.X - lastState.X, current.Y - lastState.Y));
+            if (currentEvent != null && MouseEvent != null)
+            {
+                MouseEvent(this, new MouseEventArgs(currentEvent));
+            }
+
+            lastState = current;
+        }
+    }
+}
