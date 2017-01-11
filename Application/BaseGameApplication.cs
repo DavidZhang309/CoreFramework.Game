e92c52c1d3d8a4c4d277cd513f558e4c488d2f3b
David Zhang <david_zhang_309@hotmail.com>
2017-01-10 21:39:33 -0500

    Added BaseGameApplication which handles application handling within the game object

---
+++ Application/BaseGameApplication.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace CoreFramework.Game.Application
+{
+    public abstract class BaseGameApplication : Microsoft.Xna.Framework.Game
+    {
+        public IApplication App { get; protected set; }
+        protected SpriteBatch Batch { get; private set; }
+
+        /// <summary>
+        /// Note: Must be called after LoadContent (spritebatch needs to be initialized)
+        /// </summary>
+        /// <param name="application"></param>
+        public void LoadApp(IApplication application)
+        {
+            App = application;
+            App.Load(Batch);
+        }
+
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            Batch = new SpriteBatch(GraphicsDevice);
+
+            base.LoadContent();
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (App != null)
+            {
+                App.Update(gameTime);
+            }
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+            if (App != null)
+            {
+                App.Draw(gameTime);
+            }
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ Application/IApplication.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace CoreFramework.Game.Application
+{
+    public interface IApplication
+    {
+        void Load(SpriteBatch batch);
+        void Update(GameTime time);
+        void Draw(GameTime time);
+    }
+}
