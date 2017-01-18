1547780ed349dc3d19710f65691515644d52ee3f
David Zhang <david_zhang_309@hotmail.com>
2017-01-17 19:03:09 -0500

    Added IGridTileProperties to provide graphical information about the grid. Added TileSetGraphicsProperties which uses a TileSet to translate tileID to texture

---
+++ Graphics/Grid/TileSetGraphicsProperties.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+using CoreFramework.Game.Grid;
+
+namespace CoreFramework.Game.Graphics.Grid
+{
+    public interface IGridTileProperties
+    {
+        Point TextureSize { get; }
+        float TextureScale { get; }
+        Texture2D GetTexture(Vector2 position);
+    }
+
+    public class TileSetGridGraphicsProperties : IGridTileProperties
+    {
+        protected IGridState state;
+        protected TileSet set;
+
+        public Point TextureSize { get; private set; }
+        public float TextureScale { get; set; } = 1f;
+
+        public TileSetGridGraphicsProperties(IGridState state, TileSet set)
+        {
+            this.set = set;
+            TextureSize = set.TileSize;
+            this.state = state;
+        }
+        
+        public Texture2D GetTexture(Vector2 position)
+        {
+            return set.GetTile(state.GetTile(position)).Image;
+        }
+    }
+}
